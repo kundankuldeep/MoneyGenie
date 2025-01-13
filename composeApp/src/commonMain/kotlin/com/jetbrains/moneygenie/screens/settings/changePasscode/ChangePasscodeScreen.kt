2603891285f27684f7b9a88001d823f03dd2a0ef
Kundan <Kumar.kundan@Jeevansathi.com>
2025-01-13 22:57:34 +0530

    -- ChangePasscodeScreen Handling

--- composeApp/src/commonMain/kotlin/com/jetbrains/moneygenie/screens/settings/changePasscode/ChangePasscodeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/moneygenie/screens/settings/changePasscode/ChangePasscodeScreen.kt
@@ -6,21 +6,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import com.jetbrains.moneygenie.components.FloatingLabelEditText
 import com.jetbrains.moneygenie.components.MGButton
 import com.jetbrains.moneygenie.components.MGButtonType
 import com.jetbrains.moneygenie.components.MGScaffold
 import com.jetbrains.moneygenie.components.MainAppBar
 import com.jetbrains.moneygenie.components.VerticalSpace
+import com.jetbrains.moneygenie.theme.MGTypography
 
 /**
  * Created by Kundan on 12/01/25
@@ -28,17 +34,17 @@ import com.jetbrains.moneygenie.components.VerticalSpace
 class ChangePasscodeScreen : Screen {
     @Composable
     override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
         val viewModel: ChangePasscodeScreenModel = getScreenModel()
-        ChangePasscodeScreenContent(viewModel)
-        viewModel.initViews()
+        ChangePasscodeScreenContent(navigator, viewModel)
+        viewModel.initViews(navigator)
     }
 
     @Composable
     private fun ChangePasscodeScreenContent(
+        navigator: Navigator,
         viewModel: ChangePasscodeScreenModel
     ) {
-        val navigator = LocalNavigator.currentOrThrow
-
         val scrollState = rememberScrollState() // Manages scroll position
 
         MGScaffold(
@@ -56,11 +62,11 @@ class ChangePasscodeScreen : Screen {
                     contentAlignment = Alignment.Center
                 ) {
                     MGButton(
-                        isFullWidth = true,
+                        isFullWidth = false,
                         text = "Update Passcode",
                         type = MGButtonType.SOLID,
                         onClick = {
-
+                            viewModel.onUpdatePasscodeClick()
                         })
                 }
             }
@@ -69,16 +75,54 @@ class ChangePasscodeScreen : Screen {
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(horizontal = 20.dp, vertical = 12.dp)
+                        .padding(20.dp)
                         .verticalScroll(scrollState), // Enable vertical scrolling
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
 
-                    val spaceBetween = 6
+                    val space = 12
+
+                    // Show Security Question
+                    Text(
+                        text = "Security Question: ",
+                        style = MGTypography().bodySemiBold,
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                    VerticalSpace(4)
+                    Text(
+                        text = viewModel.securityQuestion,
+                        style = MGTypography().bodyRegular
+                    )
+
+                    // Block for security Answer
+                    VerticalSpace(space)
+                    FloatingLabelEditText(
+                        label = "Answer",
+                        value = viewModel.securityAnswer,
+                        onValueChange = { value -> viewModel.updateSecurityAnswer(value) },
+                    )
+
+                    // Block For new passcode
+                    VerticalSpace(space)
+                    FloatingLabelEditText(
+                        label = "Passcode",
+                        value = viewModel.passcode,
+                        onValueChange = { value -> viewModel.updatePasscode(value) },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+                        isPasswordField = true
+                    )
 
 
-
-                    VerticalSpace(spaceBetween)
+                    // Block For confirm new passcode
+                    VerticalSpace(space)
+                    FloatingLabelEditText(
+                        label = "Confirm Passcode",
+                        value = viewModel.confirmPasscode,
+                        onValueChange = { value -> viewModel.updateConfirmPasscode(value) },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+                        isPasswordField = true
+                    )
                 }
             }
         }
--- composeApp/src/commonMain/kotlin/com/jetbrains/moneygenie/screens/settings/changePasscode/ChangePasscodeScreenModel.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/moneygenie/screens/settings/changePasscode/ChangePasscodeScreenModel.kt
@@ -1,12 +1,86 @@
 package com.jetbrains.moneygenie.screens.settings.changePasscode
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.screenModelScope
+import cafe.adriel.voyager.navigator.Navigator
+import com.jetbrains.moneygenie.data.preferences.PreferenceKeys
+import com.jetbrains.moneygenie.data.preferences.PreferenceManager
+import kotlinx.coroutines.launch
 
 /**
  * Created by Kundan on 12/01/25
  **/
 class ChangePasscodeScreenModel : ScreenModel {
-    fun initViews() {
 
+    private lateinit var navigator: Navigator
+    var passcode by mutableStateOf("")
+
+    var confirmPasscode by mutableStateOf("")
+
+    var securityQuestion by mutableStateOf("")
+
+    var securityAnswer by mutableStateOf("")
+
+    private var savedAnswer: String? = null
+
+    fun updatePasscode(value: String) {
+        passcode = value
+    }
+
+    fun updateConfirmPasscode(value: String) {
+        confirmPasscode = value
+    }
+
+    fun updateSecurityAnswer(value: String) {
+        securityAnswer = value
+    }
+
+    fun initViews(navigator: Navigator) {
+        this.navigator = navigator
+        screenModelScope.launch {
+            securityQuestion =
+                PreferenceManager.getPreference(PreferenceKeys.SECURITY_QUESTION) ?: ""
+            savedAnswer = PreferenceManager.getPreference(PreferenceKeys.SECURITY_ANSWER) ?: ""
+        }
+    }
+
+    fun onUpdatePasscodeClick() {
+        if (validateFields()) {
+            // update the passcode
+            screenModelScope.launch {
+                PreferenceManager.savePreference(PreferenceKeys.PASSCODE, passcode)
+            }
+            // Pop current screen
+            navigator.pop()
+        }
+    }
+
+    private fun validateFields(): Boolean {
+        return when {
+            securityAnswer.isBlank() -> {
+                println("Security answer cannot be empty")
+                false
+            }
+
+            securityAnswer != savedAnswer -> {
+                println("Security answer is incorrect")
+                false
+            }
+
+            passcode.isBlank() || passcode.length < 6 -> {
+                println("Passcode must be at least 6 characters")
+                false
+            }
+
+            confirmPasscode.isBlank() || confirmPasscode != passcode -> {
+                println("Passcodes do not match")
+                false
+            }
+
+            else -> true
+        }
     }
 }
